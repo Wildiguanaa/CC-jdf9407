d02b797dcda13fd1f2e3f8c0cff8180704ac6bde
Wildiguanaa <jsolomon1288@gmail.com>
2026-09-09 19:01:21 -0400

    changing background color

--- week1/sketch.js
+++ week1/sketch.js
@@ -45,7 +45,7 @@ function pickQuote() {
 }
 
 function draw() {
-  background(255, 20, 250); // set the background color
+  background(0, 20, 250); // set the background color
   drawQuote();  // draw the quote on screen
 }
 
